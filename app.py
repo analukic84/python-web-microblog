aca2d26cea54911b1600e3674c48e7fa770216c3
gothsbl <54812482+gothsbl@users.noreply.github.com>
2021-06-21 18:04:13 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -6,30 +6,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def create_app():
     app = Flask(__name__)
-    client = MongoClient(os.environ.get("MONGODB_URL"))
+    client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.microblog
-    #entries = []
 
     @app.route("/", methods=["GET", "POST"])
     def home():
         if request.method == "POST":
             entry_content = request.form.get("content")
             formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-    #        entries.append((entry_content, formatted_date))   # add tuple
-            app.db.entries.insert({"content": entry_content, "date": formatted_date})  # add to MongoDB base
-
+            app.db.entries.insert({"content": entry_content, "date": formatted_date})
+        
         entries_with_date = [
             (
                 entry["content"],
                 entry["date"],
-                datetime.datetime.today().strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
             )
-            for entry in app.db.entries.find({})  # get content from the MongoDB base
+            for entry in app.db.entries.find({})
         ]
-
         return render_template("home.html", entries=entries_with_date)
-
+    
     return app
