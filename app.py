78f167fde57e90a32fd06d48b87e1bdbbbf60418
gothsbl <54812482+gothsbl@users.noreply.github.com>
2021-06-21 17:31:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
+import os
 import datetime
 from flask import Flask, render_template, request
 from pymongo import MongoClient
+from dotenv import load_dotenv
 
+load_dotenv()
 
 def create_app():
     app = Flask(__name__)
-    client = MongoClient("mongodb+srv://microblog:ana@microblogapplication.cljcf.mongodb.net/test")
+    client = MongoClient(os.environ.get("MONGODB_URI"))
     app.db = client.microblog
-    #entries = []
 
     @app.route("/", methods=["GET", "POST"])
     def home():
         if request.method == "POST":
             entry_content = request.form.get("content")
             formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-    #        entries.append((entry_content, formatted_date))   # add tuple
-            app.db.entries.insert({"content": entry_content, "date": formatted_date})  # add to MongoDB base
-
+            app.db.entries.insert({"content": entry_content, "date": formatted_date})
+        
         entries_with_date = [
             (
                 entry["content"],
                 entry["date"],
-                datetime.datetime.today().strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
+                datetime.datetime.strptime(entry["date"], "%Y-%m-%d").strftime("%b %d")
             )
-            for entry in app.db.entries.find({})  # get content from the MongoDB base
+            for entry in app.db.entries.find({})
         ]
-
         return render_template("home.html", entries=entries_with_date)
-
+    
     return app
